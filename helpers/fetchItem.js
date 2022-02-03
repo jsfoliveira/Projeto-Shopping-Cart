965df38689a5c5a7c6ed3c63a7427505aa87c729
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-02-03 15:05:30 -0300

    requisito 9

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,9 +1,15 @@
-const fetchItem = () => {
-  // seu código aqui
-};
-
-if (typeof module !== 'undefined') {
-  module.exports = {
-    fetchItem,
+const fetchItem = (item) => {
+  // REQUISITO 2 PARTE 1
+  // RESOLUÇÃO: No código abaixo, estou chamando a Fetch API e passando o URL para a Random User API. Então, uma resposta é recebida. No entanto, a resposta recebida não é JSON, mas um objeto com uma série de métodos que podem ser usados dependendo do que quero fazer com as informações. Para converter o objeto retornado em JSON, usei o data.json(). 
+  const ItemID = item;
+  const url = `https://api.mercadolibre.com/items/${ItemID}`;
+  
+  return fetch(url)   
+    .then((data) => data.json());
   };
-}
+  
+  if (typeof module !== 'undefined') {
+    module.exports = {
+      fetchItem,
+    };
+  }
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,33 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+// REQUISITO 9 - FEITO
+  it('Teste se fetchItem é uma função', async () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+
+  it('Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada', async () => {
+    expect.assertions(1);
+    const response = await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint pedido', async () => {
+    const response = await fetchItem('MLB1615760527');
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1615760527';
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+
+  it('Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo', async () => {
+    const response = await fetchItem('MLB1615760527');
+    expect(response).toBe(item);
+  });
+
+  it('Teste se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
+      try {
+      await fetchItem();
+    } catch (error) {
+      return new Error ('You must provide an url')
+    }
+  }); 
 });
