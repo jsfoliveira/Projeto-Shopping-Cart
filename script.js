0326cd1ee4ef0ae1e63c218b60ea4ab3deafa642
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-02-03 18:12:34 -0300

    requisitos 2 e 3

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+// Criando a função que vai definir o que vai conter e a classe da image
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
+// Criando a função que vai definir o que vai conter e a classe do sku e name
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
+// Função que cria a seção que vão conter todos os produtos
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -23,10 +26,12 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 function cartItemClickListener(event) {
+// Função queu, ao clicar no li, apaga o li
 // REQUISITO 3
 const cartList = document.querySelector('.cart__items');
 cartList.removeChild(event.target);
 }
+// Função que cria os li 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -34,21 +39,22 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
-
-window.onload = () => {
-  // REQUISITO 1 PARTE 2 - OK
-  // RESOLUÇÃO: Percorri os results do produto computador e criei um novo objeto contendo o sku, name e image, esse sendo filho do elemento pai que contém a classe item.
-  const computador = fetchProducts('computador');
-  computador.then((data) => {
-    data.results.forEach((element) => {
-      const novoObjeto = {
-        sku: element.id,
-        name: element.title,
-        image: element.thumbnail,
-      };
-      document
-        .querySelector('.items')
-        .appendChild(createProductItemElement(novoObjeto));
-    });
+ const carrinho = async (event) => {
+  const getSku = getSkuFromProductItem(event.target.parentElement);
+  const data = await fetchItem(getSku);
+  const objeto = { sku: data.id, name: data.title, salePrice: data.price };
+  document.querySelector('.cart__items').appendChild(createCartItemElement(objeto));
+};
+// REQUISITO 2
+window.onload = async () => {
+  await fetchProducts('computador')
+    .then((data) => {
+      data.results.forEach((item) => {
+        const objeto = { sku: item.id, name: item.title, image: item.thumbnail };
+        const pegarItem = document.querySelector('.items');
+        pegarItem.appendChild(createProductItemElement(objeto));
+      });
   });
+  const btnAdd = document.querySelectorAll('.item__add');
+  btnAdd.forEach((button) => button.addEventListener('click', carrinho));
 };
