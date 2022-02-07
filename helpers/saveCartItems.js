86ae6897760a2f40dcd27408d374a6b04cacbd4e
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-02-07 15:25:16 -0300

    requisito 4

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,7 +1,5 @@
-// REQUISITO 10 PARTE 1 - OK
-const saveCartItems = async (item) => {
-  localStorage.setItem('cartItems', item);
-};
+// REQUISITO 10 PARTE 1
+const saveCartItems = (item) => localStorage.setItem('cartItems', item);
 
 if (typeof module !== 'undefined') {
   module.exports = saveCartItems;
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const cartItems = document.querySelector('.cart__items');
+
 // Criando a função que vai definir o que vai conter e a classe da image
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -5,6 +7,7 @@ function createProductImageElement(imageSource) {
   img.src = imageSource;
   return img;
 }
+
 // Criando a função que vai definir o que vai conter e a classe do sku e name
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
@@ -12,6 +15,7 @@ function createCustomElement(element, className, innerText) {
   e.innerText = innerText;
   return e;
 }
+
 // Função que cria a seção que vão conter todos os produtos
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
@@ -22,16 +26,19 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   return section;
 }
+
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
-function cartItemClickListener(event) {
-// Função queu, ao clicar no li, apaga o li
-// REQUISITO 3 - OK
+
+// REQUISITO 3: Função que, ao clicar no li, apaga o li
 // RESOLUÇÃO: vai remover o conteúdo de todo ol (cart__items).
-const cartList = document.querySelector('.cart__items');
-cartList.removeChild(event.target);
+function cartItemClickListener(event) {
+cartItems.removeChild(event.target);
+// REQUISITO 4 - Quando eu clicar no cart__item, que ele apague do localStorage.
+saveCartItems(cartItems.innerHTML);
 }
+
 // Função que cria os li 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
@@ -40,22 +47,34 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
 // REQUISITO 2 - PARTE 1
 // RESOLUÇÃO: Eu tive ajuda de Sheila Arelo. Criei a função que vai ser responsável por adicionar produto no carrinho. Vai pegando o conteúdo do primeiro elemento do pai e vai criando um objeto com o id, title e price. Essa informação vai ser guardada em um elemento li que será filho do ol. Irá fazer isso com cada firstchild criado.
  const carrinho = async (event) => {
-  const getSku = event.target.parentElement.firstChild.innerText; // getSku quer retornar o conteúdo do primeiro filho do elemento pai do item__sku.
-  const data = await fetchItem(getSku);
+  const getSku = event.target.parentNode; // getSku quer retornar o conteúdo do primeiro filho do elemento pai do item__sku.
+  const data = await fetchItem(getSkuFromProductItem(getSku));
+  console.log(data);
   const objeto = { sku: data.id, name: data.title, salePrice: data.price };
-  const ol = document.querySelector('.cart__items');
-  ol.appendChild(createCartItemElement(objeto));
+  cartItems.appendChild(createCartItemElement(objeto));
+// REQUISITO 4 - Quando eu clicar no cart__item, que ele salve do localStorage.
+  saveCartItems(cartItems.innerHTML);
 };
+
 // REQUISITO 6 - PARTE 1
 // RESOLUÇÃO: Acessei o ol que armazena todas as li, através do cartItem. Percorri cada elemento e fui removendo.
-const apagaTudo = () => {
+const apagarTudo = () => {
   const cartItem = document.querySelectorAll('.cart__item');
   cartItem.forEach((element) => {
     element.remove();
   });
+};
+
+// REQUISITO 4:  A função getSavedCartItems deve recuperar os itens do carrinho de compras do localStorage quando carregamos a página.
+// RESOLUÇÃO: Ao clicar em cada filho do elemento cartItems, que estão no carrinho e que etambém foram salvos no localstorage, vai ativar a função cartItemClickListener.
+const recuperarItem = () => {
+  cartItems.innerHTML = getSavedCartItems();
+  console.log(cartItems); // vai aparecer os itens salvos no local storage
+  cartItems.childNodes.forEach((li) => li.addEventListener('click', cartItemClickListener));
 };
 
 // REQUISITO 1 - PARTE 2
@@ -71,8 +90,12 @@ window.onload = async () => {
   });
   const btnAdd = document.querySelectorAll('.item__add');
   btnAdd.forEach((button) => button.addEventListener('click', carrinho));
+
   // REQUISITO 6 - PARTE 2
   // RESOLUÇÃO: Acessei o botão 'Esvaziar carrinho' e adicionei um eventListener para ativar a função apagaTudo ao clicar nele.
   const empty = document.querySelector('.empty-cart');
-  empty.addEventListener('click', apagaTudo);
+  empty.addEventListener('click', apagarTudo);
+
+  // REQUISITO 4
+  recuperarItem();
 };
